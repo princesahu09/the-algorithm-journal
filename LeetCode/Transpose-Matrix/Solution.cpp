0f1b0e8a2161c27cc9367551ed8a0e332ffbb509
Prince sahu <psahu3086@gmail.com>
2025-10-21 22:43:34 +0530

    Add solution for Transpose Matrix

--- LeetCode/Transpose-Matrix/Solution.cpp
+++ LeetCode/Transpose-Matrix/Solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    vector<vector<int>> transpose(const vector<vector<int>>& matrix) {
         int n = matrix.size();        // rows
-        int m = matrix[0].size();     // cols
+        int m = matrix[0].size();     // columns
+        
+       
+        vector<vector<int>> result(m, vector<int>(n));
 
-        vector<vector<int>> transpose(m, vector<int>(n));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                transpose[j][i] = matrix[i][j];
+      
+        for (int i = 0; i < n; ++i) {
+            const vector<int>& row = matrix[i];   
+            for (int j = 0; j < m; ++j) {
+                result[j][i] = row[j];
             }
         }
-
-        return transpose;
+        return result;
     }
 };
