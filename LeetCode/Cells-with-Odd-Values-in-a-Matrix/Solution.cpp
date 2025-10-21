7c54cb56a36608afa10c2e2a50c04b21d8c7377b
Prince sahu <psahu3086@gmail.com>
2025-10-21 23:23:08 +0530

    Add solution for Cells with Odd Values in a Matrix

--- LeetCode/Cells-with-Odd-Values-in-a-Matrix/Solution.cpp
+++ LeetCode/Cells-with-Odd-Values-in-a-Matrix/Solution.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int oddCells(int m, int n, vector<vector<int>>& indices) {
-        vector<vector<int>> matrix(m, vector<int>(n));
-
-        int i = 0;
-        while (i < indices.size()) {
-
-            int row = indices[i][0];
-            int col = indices[i][1];
-
-            for (int j = 0; j < n; j++) {
-                matrix[row][j] += 1;
-            }
-            for (int k = 0; k < m; k++) {
-                matrix[k][col] += 1;
-            }
-            i++;
+        vector<int>rowcount(m,0);
+        vector<int>colcount(n,0);
+        for(auto &index:indices)
+        {
+            rowcount[index[0]]++;
+            colcount[index[1]]++;
         }
 
-        int odd = 0;
+
+          int odd = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] % 2 != 0)
+                if ((rowcount[i]+colcount[j]) % 2 != 0)
                     odd++;
             }
         }
         return odd;
+
+
+        
     }
 };
