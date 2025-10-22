14e99e125ea75cd3097b0460ea231584bb7ce742
Prince sahu <psahu3086@gmail.com>
2025-10-22 14:51:17 +0530

    Add solution for Single Element in a Sorted Array

---
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+       
+        int unique1=0;
+        int unique2=0;
+
+        for(int i=0;i<nums.size();i+=2)
+        {
+            unique1^=nums[i];
+
+        }
+        for(int i=1;i<nums.size();i+=2)
+        {
+            unique2^=nums[i];
+        }
+
+        return unique1^unique2;
+        
+    }
+};
