35d87f61d768595a37751029a905ece62e8ecc06
Prince sahu <psahu3086@gmail.com>
2025-10-24 01:07:57 +0530

    Add solution for Decode XORed Array

---
+++ LeetCode/Decode-XORed-Array/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+
+        vector<int>ans;
+        ans.push_back(first);
+        
+
+        for(int i=0;i<encoded.size();i++)
+        {
+            int b=first^encoded[i];
+            ans.push_back(b);
+            first=b;
+            
+
+
+
+        }
+
+        return ans;
+        
+    }
+};
