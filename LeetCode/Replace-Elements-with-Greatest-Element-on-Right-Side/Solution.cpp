6416015f545e5f77c582d5f4dc6356201d343c20
Prince sahu <psahu3086@gmail.com>
2025-10-23 23:27:20 +0530

    Add solution for Replace Elements with Greatest Element on Right Side

--- LeetCode/Replace-Elements-with-Greatest-Element-on-Right-Side/Solution.cpp
+++ LeetCode/Replace-Elements-with-Greatest-Element-on-Right-Side/Solution.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
 
-        for(int i=0;i+1<arr.size();i++)
-        {
-           auto it=max_element(arr.begin()+i+1,arr.end());
-           arr[i]=*it;
-        }
-        arr[arr.size()-1]=-1;
-        return arr;
+      int big=arr[arr.size()-1];
+
+      for(int i=arr.size()-2;i>=0;i--)
+      {
+        int temp=arr[i];
+        arr[i]=big;
+        big=max(big,temp);
+      }
+      arr[arr.size()-1]=-1;
+      return arr;
+
         
     }
 };
