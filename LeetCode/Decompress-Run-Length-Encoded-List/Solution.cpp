d3f272309f4fe6e79818f3200081d01688b31bed
Prince sahu <psahu3086@gmail.com>
2025-10-23 20:29:38 +0530

    Add solution for Decompress Run-Length Encoded List

---
+++ LeetCode/Decompress-Run-Length-Encoded-List/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> res;
+        int n = nums.size();
+
+        for (int i=0; i<n; i+=2){
+            int freq = nums[i];
+            int val = nums[i+1];
+            while(freq-- > 0) res.push_back(val);
+        }
+
+        return res;
+    }
+};
