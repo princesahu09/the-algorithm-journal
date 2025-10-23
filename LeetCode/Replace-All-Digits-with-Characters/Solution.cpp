810413ba73ab79106565360142187bb205925324
Prince sahu <psahu3086@gmail.com>
2025-10-24 03:02:23 +0530

    Add solution for Replace All Digits with Characters

---
+++ LeetCode/Replace-All-Digits-with-Characters/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+
+        
+
+        for(int i=1;i<s.size();i++)
+        {
+            if(i%2!=0)
+           {s[i]=s[i-1]+(s[i]-'0');}
+        }
+        return s;
+        
+    }
+};
