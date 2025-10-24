ede6e1ccfa818f58eb19c582e6824ec0d061baa9
Prince sahu <psahu3086@gmail.com>
2025-10-25 00:50:26 +0530

    Add solution for Merge Strings Alternately

--- LeetCode/Merge-Strings-Alternately/Solution.cpp
+++ LeetCode/Merge-Strings-Alternately/Solution.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
 
-        
+        int n1=word1.size();
+        int n2=word2.size();
         string ans="";
-        ans.reserve(word1.size()+word2.size());
+        ans.reserve(n1+n2);
         int i=0;
         int j=0;
-        while(i<word1.size()||j<word2.size())
+        while(i<n1||j<n2)
 
         {
-           if(i<word1.size())ans+=word1[i++];
-           if(j<word2.size())ans+=word2[j++];
+           if(i<n1)ans+=word1[i++];
+           if(j<n2)ans+=word2[j++];
         }
        
 
