a697b1cf4aee103e3cee2ed8dad29c6efd23a62c
Prince sahu <psahu3086@gmail.com>
2025-10-28 17:26:32 +0530

    Add solution for Number of Strings That Appear as Substrings in Word

---
+++ LeetCode/Number-of-Strings-That-Appear-as-Substrings-in-Word/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int ans=0;
+
+
+        for(auto &i:patterns)
+        {
+            if(word.find(i)!=string::npos)
+            {
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
