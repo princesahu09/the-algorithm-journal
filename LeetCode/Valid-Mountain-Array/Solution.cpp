7113a8b0783b6cb8640b03dedb97279d8bec3978
Prince sahu <psahu3086@gmail.com>
2025-10-30 10:57:35 +0530

    Add solution for Valid Mountain Array

--- LeetCode/Valid-Mountain-Array/Solution.cpp
+++ LeetCode/Valid-Mountain-Array/Solution.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
+        const int n=arr.size();
 
-        if (arr.size() < 3)
+        if (n< 3)
             return false;
         int i = 0;
 
-        for (; i+1 < arr.size(); i++) {
+        for (; i+1 < n; i++) {
             if (arr[i] < arr[i + 1]) {
                 continue;
             } else {
                 break;
             }
         }
-        if(i==arr.size()-1||i==0)
+        if(i==n-1||i==0)
         {
             return false;
         }
-        for (; i +1< arr.size(); i++) {
+        for (; i +1< n; i++) {
             if (arr[i] > arr[i + 1]) {
                 continue;
             } else {
