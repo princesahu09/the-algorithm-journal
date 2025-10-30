8e4dd9e53ff1aeb8ea5000c27413bb341025151f
Prince sahu <psahu3086@gmail.com>
2025-10-30 21:29:56 +0530

    Add solution for N-th Tribonacci Number

---
+++ LeetCode/N-th-Tribonacci-Number/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0||n==1)return n;
+        if(n==2)return 1;
+
+        int x=0;
+        int y=1;
+        int z=1;
+        
+
+        for(int i=3;i<=n;i++)
+        {
+            int curr=x+y+z;
+            x=y;
+            y=z;
+            z=curr;
+        }
+        return z;
+        
+    }
+};
