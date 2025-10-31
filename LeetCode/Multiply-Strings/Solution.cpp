99106b0377ab7a59822ed3fb2387ed49cc36cfc7
Prince sahu <psahu3086@gmail.com>
2025-11-01 02:46:34 +0530

    Add solution for Multiply Strings

--- LeetCode/Multiply-Strings/Solution.cpp
+++ LeetCode/Multiply-Strings/Solution.cpp
@@ -1,74 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    string add(string s1, string s2) {
-        if (s1 == "" || s2 == "")
-            return s1 + s2;
+    string add(const string &a, const string &b) {
+        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        string res;
+        res.reserve(max(a.size(), b.size()) + 1);
 
-        string ans = "";
-        int i = s1.size() - 1;
-        int j = s2.size() - 1;
-        int carry = 0;
-        while (i >= 0 || j >= 0 || carry > 0) {
+        while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
-
-            if (i >= 0)
-                sum += (s1[i--] - '0');
-            if (j >= 0)
-                sum += (s2[j--] - '0');
-            ans.push_back(sum % 10 + '0');
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            res.push_back(sum % 10 + '0');
             carry = sum / 10;
         }
-        if (carry > 0) {
-            ans.push_back(carry + '0');
-        }
-        reverse(ans.begin(), ans.end());
-
-        return ans;
+        reverse(res.begin(), res.end());
+        return res;
     }
-    string multiplication(char s1, string s2) {
-        if (s1 == '1')
-            return s2;
-        else if (s1 == '0')
-            return "0";
 
-        string ans = "";
-        int i = s2.size() - 1;
+    string multiplication(char c, const string &num) {
+        if (c == '0') return "0";
+        if (c == '1') return num;
+
         int carry = 0;
+        string res;
+        res.reserve(num.size() + 1);
 
-        while (i >= 0) {
-            int product = (s2[i] - '0') * (s1 - '0') + carry;
-            if (product > 9) {
-                carry = product / 10;
-            } else {
-                carry = 0;
-            }
-            ans.push_back(product % 10 + '0');
-            i--;
+        for (int i = num.size() - 1; i >= 0; i--) {
+            int prod = (num[i] - '0') * (c - '0') + carry;
+            res.push_back(prod % 10 + '0');
+            carry = prod / 10;
         }
-        if (carry > 0) {
-            ans.push_back(carry + '0');
-        }
-        reverse(ans.begin(), ans.end());
 
-        return ans;
+        if (carry) res.push_back(carry + '0');
+        reverse(res.begin(), res.end());
+        return res;
     }
+
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0")
-            return "0";
+        if (num1 == "0" || num2 == "0") return "0";
 
-        string ans = "";
-        int n1 = num1.size() - 1;
-        int zero = 0;
+        int n = num1.size(), m = num2.size();
+        vector<int> product(n + m, 0);
 
-        int i = n1;
-        while (i >= 0) {
-            string curr = multiplication(num1[i], num2);
-            curr.append(zero, '0');
-            zero++;
-            ans = add(curr, ans);
-
-            i--;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = product[i + j + 1] + mul;
+                product[i + j + 1] = sum % 10;
+                product[i + j] += sum / 10;
+            }
         }
-        return ans;
+
+        string ans;
+        ans.reserve(n + m);
+        int i = 0;
+        while (i < product.size() && product[i] == 0) i++; 
+        for (; i < product.size(); i++) ans.push_back(product[i] + '0');
+
+        return ans.empty() ? "0" : ans;
     }
 };
