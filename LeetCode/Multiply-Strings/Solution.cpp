7c1fdf65d00d182fb2a9051d5987299c24b13f6d
Prince sahu <psahu3086@gmail.com>
2025-11-01 02:47:33 +0530

    Add solution for Multiply Strings

--- LeetCode/Multiply-Strings/Solution.cpp
+++ LeetCode/Multiply-Strings/Solution.cpp
@@ -3,40 +3,40 @@ using namespace std;
 
 class Solution {
 public:
-    string add(const string &a, const string &b) {
-        int i = a.size() - 1, j = b.size() - 1, carry = 0;
-        string res;
-        res.reserve(max(a.size(), b.size()) + 1);
+    // string add(const string &a, const string &b) {
+    //     int i = a.size() - 1, j = b.size() - 1, carry = 0;
+    //     string res;
+    //     res.reserve(max(a.size(), b.size()) + 1);
 
-        while (i >= 0 || j >= 0 || carry) {
-            int sum = carry;
-            if (i >= 0) sum += a[i--] - '0';
-            if (j >= 0) sum += b[j--] - '0';
-            res.push_back(sum % 10 + '0');
-            carry = sum / 10;
-        }
-        reverse(res.begin(), res.end());
-        return res;
-    }
+    //     while (i >= 0 || j >= 0 || carry) {
+    //         int sum = carry;
+    //         if (i >= 0) sum += a[i--] - '0';
+    //         if (j >= 0) sum += b[j--] - '0';
+    //         res.push_back(sum % 10 + '0');
+    //         carry = sum / 10;
+    //     }
+    //     reverse(res.begin(), res.end());
+    //     return res;
+    // }
 
-    string multiplication(char c, const string &num) {
-        if (c == '0') return "0";
-        if (c == '1') return num;
+    // string multiplication(char c, const string &num) {
+    //     if (c == '0') return "0";
+    //     if (c == '1') return num;
 
-        int carry = 0;
-        string res;
-        res.reserve(num.size() + 1);
+    //     int carry = 0;
+    //     string res;
+    //     res.reserve(num.size() + 1);
 
-        for (int i = num.size() - 1; i >= 0; i--) {
-            int prod = (num[i] - '0') * (c - '0') + carry;
-            res.push_back(prod % 10 + '0');
-            carry = prod / 10;
-        }
+    //     for (int i = num.size() - 1; i >= 0; i--) {
+    //         int prod = (num[i] - '0') * (c - '0') + carry;
+    //         res.push_back(prod % 10 + '0');
+    //         carry = prod / 10;
+    //     }
 
-        if (carry) res.push_back(carry + '0');
-        reverse(res.begin(), res.end());
-        return res;
-    }
+    //     if (carry) res.push_back(carry + '0');
+    //     reverse(res.begin(), res.end());
+    //     return res;
+    // }
 
     string multiply(string num1, string num2) {
         if (num1 == "0" || num2 == "0") return "0";
