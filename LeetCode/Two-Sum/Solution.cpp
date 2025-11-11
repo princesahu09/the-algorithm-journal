1ccdd7d4990a69da79828bd3af3f19b4f67717b4
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-11 19:27:28 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,30 +1,33 @@
+#include<vector>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
 
-        unordered_map<int, int> mapping;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (!mapping.count(nums[i])) {
-                mapping[nums[i]] = i;
+        for(int i=0;i<nums.size();i++)
+        {
+            int sum=0;
+            for(int j=i+1;j<nums.size();j++)
+            {
+                sum=nums[i]+nums[j];
+                if(sum==target)
+                {
+                    ans.insert(ans.begin(),i);
+                    ans.insert(ans.begin(),j);
+                    break;
+                    
+                }
             }
-        }
-
-        vector<int> ans(2);
-
-        for (int i = 0; i < nums.size(); i++) {
-            int less = target - nums[i];
-
-            if (mapping.count(less) == 1 && mapping[less] != i) {
-                ans[0]=i;
-                ans[1]=mapping[less];
-                break;
-            } else {
+            if(ans.empty())
+            {
                 continue;
             }
+            else
+            {
+                break;
+            }
         }
-
         return ans;
+        
     }
 };
