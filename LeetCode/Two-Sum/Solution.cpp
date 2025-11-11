55464479730554ecb5ffd818625e9573bed6063d
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-11 19:27:16 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -11,14 +11,14 @@ public:
             }
         }
 
-        vector<int> ans;
+        vector<int> ans(2);
 
         for (int i = 0; i < nums.size(); i++) {
             int less = target - nums[i];
 
             if (mapping.count(less) == 1 && mapping[less] != i) {
-                ans.push_back(i);
-                ans.push_back(mapping[less]);
+                ans[0]=i;
+                ans[1]=mapping[less];
                 break;
             } else {
                 continue;
