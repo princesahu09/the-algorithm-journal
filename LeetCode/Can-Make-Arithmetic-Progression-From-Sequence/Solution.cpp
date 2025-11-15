23bfcc8d438fd0b0239e72dcf767d2df85572610
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-15 19:58:18 +0530

    Add solution for Can Make Arithmetic Progression From Sequence

--- LeetCode/Can-Make-Arithmetic-Progression-From-Sequence/Solution.cpp
+++ LeetCode/Can-Make-Arithmetic-Progression-From-Sequence/Solution.cpp
@@ -5,9 +5,9 @@ public:
         sort(arr.begin(),arr.end());
         int cd=arr[1]-arr[0];
 
-        for(int i=1;i<arr.size();i++)
+        for(int i=0;i+1<arr.size();i++)
         {
-            if(arr[i]-arr[i-1]!=cd)
+            if(arr[i+1]-arr[i]!=cd)
             {
                 return false;
             }
