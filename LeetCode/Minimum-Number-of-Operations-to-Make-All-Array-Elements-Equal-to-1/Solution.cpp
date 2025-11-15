daea91b2d7f363ea3056b95d3a5d3c7ad5ad6f3d
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-15 20:54:57 +0530

    Add solution for Minimum Number of Operations to Make All Array Elements Equal to 1

---
+++ LeetCode/Minimum-Number-of-Operations-to-Make-All-Array-Elements-Equal-to-1/Solution.cpp
@@ -0,0 +1,57 @@
+class Solution {
+
+private:
+   
+    bool findGcd(vector<int>& nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = gcd(nums[i], ans);
+        }
+        return ans == 1;
+    }
+
+public:
+    int minOperations(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+       
+        if (!findGcd(nums)) return -1;
+
+        
+        int one = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 1)
+                one++;
+        }
+
+      
+        if (one > 0)
+        {
+            return n - one;
+        }
+
+       
+        int best = INT_MAX;
+
+        for (int i = 0; i < n; i++)
+        {
+            int g = 0;
+            for (int j = i; j < n; j++)
+            {
+                g = gcd(g, nums[j]);
+                if (g == 1)
+                {
+                    best = min(best, j - i);
+                    break;
+                }
+            }
+        }
+
+        
+        return best + (n - 1);
+    }
+};
