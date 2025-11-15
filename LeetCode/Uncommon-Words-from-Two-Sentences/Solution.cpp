fa5d8bb726c9e9e89ca2156b8b0261370b34cd2e
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-16 04:19:12 +0530

    Add solution for Uncommon Words from Two Sentences

--- LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
+++ LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
 
-        vector<string>ans;
         unordered_map<string,int>disjoint;
 
-        stringstream ss(s1);
+        stringstream ss(s1+" "+s2);
         string word;
         while(ss>>word)
         {
             disjoint[word]++;
 
         }
-        stringstream sb(s2);
-        while(sb>>word)
-        {
-            disjoint[word]++;
-        }
+        vector<string>ans;
+        ans.reserve(disjoint.size());
+        
+        
 
         for(auto &i:disjoint)
         {
