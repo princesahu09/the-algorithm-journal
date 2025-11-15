e0c3462e0da8aaf73e103fbcf6c400a0eb9aecc4
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-15 17:24:01 +0530

    Add solution for Ugly Number III

---
+++ LeetCode/Ugly-Number-III/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+private:
+    long long countUgly(long long n, long long a, long long b, long long c)
+    {
+        long long ab = lcm(a, b);
+        long long bc = lcm(b, c);
+        long long ac = lcm(a, c);
+        long long abc = lcm(ab, c);
+
+        return n / a + n / b + n / c
+             - n / ab - n / bc - n / ac
+             + n / abc;
+    }
+
+public:
+    int nthUglyNumber(int n, int a, int b, int c) {
+        
+        long long low = 1, high = 2e18;  
+        
+        while (low < high)
+        {
+            long long mid = low + (high - low) / 2;
+            long long cnt = countUgly(mid, a, b, c);
+
+            if (cnt >= n)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
