e4e29d9f0992b94ff19cd9c6e4cef566272b11b1
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-16 14:50:36 +0530

    Add solution for Mean of Array After Removing Some Elements

--- LeetCode/Mean-of-Array-After-Removing-Some-Elements/Solution.cpp
+++ LeetCode/Mean-of-Array-After-Removing-Some-Elements/Solution.cpp
@@ -4,8 +4,8 @@ public:
         const int n=arr.size();
         double mean=0.0;
 
-        int remover=n/20;
-        int deno=n-2*remover;
+        const int remover=n/20;
+        const int deno=n-2*remover;
         sort(arr.begin(),arr.end());
         long long sum=accumulate(arr.begin()+remover,arr.end()-remover,0LL);
         mean=(double)sum/deno;
