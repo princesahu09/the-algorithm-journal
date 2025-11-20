5d3fa5590024a3e00793a827809ddcd3d5a43963
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-21 03:19:34 +0530

    Add solution for Backspace String Compare

--- LeetCode/Backspace-String-Compare/Solution.cpp
+++ LeetCode/Backspace-String-Compare/Solution.cpp
@@ -2,33 +2,28 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
 
-        stack<char>a,b;
+        string a = "";
+        string b = "";
 
-        for(auto &i:s)
-        {
-            if(i=='#'&&!a.empty())
-            {
-                a.pop();
-            }
-            else if(i!='#')
-            {
-                a.push(i);
+        for (auto& i : s) {
+            if (i != '#') {
+                a.push_back(i);
+            } else {
+                if (!a.empty()) {
+                    a.pop_back();
+                }
             }
         }
-         for(auto &i:t)
-        {
-            if(i=='#'&&!b.empty())
-            {
-                b.pop();
-            }
-            else if(i!='#')
-            {
-                b.push(i);
+         for (auto& i : t) {
+            if (i != '#') {
+                b.push_back(i);
+            } else {
+                if (!b.empty()) {
+                    b.pop_back();
+                }
             }
         }
 
-        return a==b;
-
-        
+        return a == b;
     }
 };
