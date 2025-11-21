5877df0ec4470a5c6eb49f1a66c7221df96c5af6
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-22 04:58:03 +0530

    Add solution for Adding Spaces to a String

--- LeetCode/Adding-Spaces-to-a-String/Solution.cpp
+++ LeetCode/Adding-Spaces-to-a-String/Solution.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        queue<string>q;
-        int start=0;
-        string ans="";
+        string ans = "";
+        int a=0;
 
-        for(int i=0;i<spaces.size();i++)
-        {
-            int len=spaces[i]-start;
-            string str=s.substr(start,len);
-           q.push(str);
-           start=spaces[i];
-
+        for (int i = 0; i < s.size(); i++) {
+            if (a<spaces.size()&&i == spaces[a]) {
+                ans += " ";
+                a++;
+            }
+            ans+=s[i];
         }
-        q.push(s.substr(start));
-        while(!q.empty())
-        {
-            ans+=q.front()+" ";
-            q.pop();
-        }
-        ans.pop_back();
         return ans;
     }
 };
