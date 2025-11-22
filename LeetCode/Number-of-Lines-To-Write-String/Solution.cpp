f7a314b61503dcecc11ea5396ca4e190540c2c6d
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-22 21:03:08 +0530

    Add solution for Number of Lines To Write String

--- LeetCode/Number-of-Lines-To-Write-String/Solution.cpp
+++ LeetCode/Number-of-Lines-To-Write-String/Solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-        int line = 1;
-
+        int line = 1;              
+        int sum = 100;              
         const int n = s.size();
-        int sum = 100;
 
         for (int i = 0; i < n; i++) {
             char ch = s[i];
             int index = ch - 'a';
+
+            
             if (sum - widths[index] >= 0) {
                 sum -= widths[index];
-            } else {
+            } 
+            else {                       
                 line++;
                 sum = 100 - widths[index];
             }
