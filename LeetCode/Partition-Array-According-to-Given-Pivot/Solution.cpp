9186ed068d7318dd011f1ca424d89756d4088f25
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-22 17:09:12 +0530

    Add solution for Partition Array According to Given Pivot

--- LeetCode/Partition-Array-According-to-Given-Pivot/Solution.cpp
+++ LeetCode/Partition-Array-According-to-Given-Pivot/Solution.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>a1;
-        vector<int>a2;
-        vector<int>a3;
-        
-        for(auto &i:nums)
-        {
-            if(i>pivot)
+        const int n = nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n - 1;
+
+        for (auto &i : nums) {
+           
+             if(i<pivot)
             {
-                a3.push_back(i);
-            }
-            else if(pivot==i)
-            {
-                a2.push_back(i);
-            }
-            else
-            {
-                a1.push_back(i);
+                ans[left++]=i;
             }
         }
-
-        a1.insert(a1.end(),a2.begin(),a2.end());
-        a1.insert(a1.end(),a3.begin(),a3.end());
-
-        return a1;
         
+        for (auto &i : nums) {
+           
+             if(i==pivot)
+            {
+                ans[left++]=i;
+            }
+        }
+        for (auto &i : nums) {
+           
+             if(i>pivot)
+            {
+                ans[left++]=i;
+            }
+        }
+       
+
+        return ans;
     }
 };
