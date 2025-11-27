b5b77f41f39348c22096ddcedc74430d6ebeaccd
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-27 18:57:25 +0530

    Add solution for 2 Keys Keyboard

--- LeetCode/2-Keys-Keyboard/Solution.cpp
+++ LeetCode/2-Keys-Keyboard/Solution.cpp
@@ -1,26 +1,26 @@
-class Solution {
-public:
-    int minSteps(int n) {
-        int ans=0;
-        int d=2;
-
-        if(n<2)return 0;
-        
-        while(n>1)
-        {
-            if(n%d==0)
-            {
-                ans+=d;
-                n/=d;
-            }
-            else
-            {
-               d++;
-            }   
-
-        }
-
-        return ans;
-        
-    }
-};
+1class Solution {
+2public:
+3    int minSteps(int n) {
+4        int ans=0;
+5        int d=2;
+6
+7        if(n<2)return 0;
+8        
+9        while(n>1)
+10        {
+11            if(n%d==0)
+12            {
+13                ans+=d;
+14                n/=d;
+15            }
+16            else
+17            {
+18               d++;
+19            }   
+20
+21        }
+22
+23        return ans;
+24        
+25    }
+26};
