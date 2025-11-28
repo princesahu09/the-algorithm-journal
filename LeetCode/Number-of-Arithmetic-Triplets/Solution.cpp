089fb00c1edd3b662098e2e9971e09686855c68e
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-28 07:35:37 +0530

    Add solution for Number of Arithmetic Triplets

--- LeetCode/Number-of-Arithmetic-Triplets/Solution.cpp
+++ LeetCode/Number-of-Arithmetic-Triplets/Solution.cpp
@@ -1,26 +1,21 @@
 1class Solution {
 2public:
 3    int arithmeticTriplets(vector<int>& nums, int diff) {
-4        const int n=nums.size();
+4      
 5        int ans=0;
-6
+6      const  unordered_set<int>s(nums.begin(),nums.end());
 7
-8        for(int i=0;i<n;i++)
+8        for(auto &x:nums)
 9        {
-10            for(int j=i+1;j<n;j++)
+10            if(s.count(x+diff)&&s.count(x+2*diff))
 11            {
-12                for(int k=j+1;k<n;k++)
-13                {
-14
-15                    if((nums[j]-nums[i])==diff&&(nums[k]-nums[j])==diff)
-16                    {
-17                        ans++;
-18                    }
-19                }
-20            
-21            }
-22        }
-23        return ans;
-24        
-25    }
-26};
+12                ans++;
+13            }
+14        }
+15
+16
+17      
+18        return ans;
+19        
+20    }
+21};
