3259194cbc9b3e4aff78ca19365bc4bb50d348a5
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-02 02:31:50 +0530

    Add solution for Binary Tree Level Order Traversal II

--- LeetCode/Binary-Tree-Level-Order-Traversal-II/Solution.cpp
+++ LeetCode/Binary-Tree-Level-Order-Traversal-II/Solution.cpp
@@ -11,47 +11,34 @@
 11 */
 12class Solution {
 13
-14    private: void solve(queue<TreeNode*>q,vector<vector<int>>&ans)
+14    private: void solve(TreeNode* root,vector<vector<int>>&ans,int level)
 15    {
 16
-17        vector<int>level;
-18          while(!q.empty())
-19          {
-20            TreeNode* temp=q.front();
-21            q.pop();
-22             
-23             if(temp==nullptr)
-24             {
-25                ans.push_back(level);
-26                level.clear();
-27
-28                if(!q.empty())
-29                {
-30                    q.push(nullptr);
-31                }
-32             }
-33
-34             else
-35             {
-36                level.push_back(temp->val);
-37
-38                if(temp->left)q.push(temp->left);
-39                if(temp->right)q.push(temp->right);
-40             }
-41          }
-42
-43          reverse(ans.begin(),ans.end());
-44    }
-45public:
-46    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-47
-48        queue<TreeNode*>q;
-49        vector<vector<int>>ans;
-50        if(root==nullptr)return ans;
-51        q.push(root);
-52        q.push(nullptr);
-53        solve(q,ans);
-54        return ans;
-55        
-56    }
-57};
+17        if(root==nullptr)return;
+18
+19        if(level==ans.size())
+20        {
+21            ans.push_back({});
+22        }
+23
+24        ans[level].push_back(root->val);
+25
+26        solve(root->left,ans,level+1);
+27        solve(root->right,ans,level+1);
+28
+29
+30       
+31    }
+32public:
+33    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+34
+35     
+36        vector<vector<int>>ans;
+37        if(root==nullptr)return ans;
+38      
+39        solve(root,ans,0);
+40        reverse(ans.begin(),ans.end());
+41        return ans;
+42        
+43    }
+44};
