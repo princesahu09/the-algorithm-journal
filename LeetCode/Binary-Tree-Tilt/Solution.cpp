b016948ae579818ee01af828c3666f161bfbadc0
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-02 12:05:58 +0530

    Add solution for Binary Tree Tilt

---
+++ LeetCode/Binary-Tree-Tilt/Solution.cpp
@@ -0,0 +1,43 @@
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13class Solution {
+14
+15private:
+16    int total(TreeNode* root, int sum) {
+17        if (root == nullptr)
+18            return sum;
+19
+20        return root->val + total(root->left, sum) + total(root->right, sum);
+21    }
+22    void solve(TreeNode* root, vector<int>& tilt) {
+23        if (root == nullptr)
+24            return;
+25
+26        int leftsum = total(root->left,0);
+27        int rightsum = total(root->right,0);
+28
+29        tilt.push_back(abs(leftsum - rightsum));
+30
+31        solve(root->left, tilt);
+32        solve(root->right, tilt);
+33    }
+34
+35public:
+36    int findTilt(TreeNode* root) {
+37
+38        vector<int> tilt;
+39        solve(root, tilt);
+40
+41        return accumulate(tilt.begin(), tilt.end(), 0);
+42    }
+43};
