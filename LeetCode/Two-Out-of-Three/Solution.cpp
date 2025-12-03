003bdcdb701b8939c062720e1f10001fa2bcdb42
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-04 00:53:01 +0530

    Add solution for Two Out of Three

--- LeetCode/Two-Out-of-Three/Solution.cpp
+++ LeetCode/Two-Out-of-Three/Solution.cpp
@@ -1,30 +1,32 @@
-1    class Solution {
-2    public:
-3        vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
-4            
+1class Solution {
+2public:
+3    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2,
+4                              vector<int>& nums3) {
 5
-6            vector<int >ans;
-7            unordered_map<int ,int>mask;
-8            for(int &i:nums1)
-9            {
-10                mask[i]=1;
-11            }
-12            for(int &i:nums2)
-13            {
-14                mask[i]|=(1<<1);
-15            } for(int &i:nums3)
-16            {
-17                mask[i]|=(1<<2);
-18            }
-19
-20            for(auto &i:mask)
-21            {
-22                if(__builtin_popcount(i.second)>=2)
-23                {
-24                    ans.push_back(i.first);
-25                }
-26            }
-27            
-28            return ans;
-29        }
-30    };
+6        unordered_set<int> ans;
+7
+8        unordered_set<int> num11(nums1.begin(), nums1.end());
+9        unordered_set<int> num22(nums2.begin(), nums2.end());
+10        unordered_set<int> num33(nums3.begin(), nums3.end());
+11
+12        for (int& i : nums1) {
+13            if (num22.count(i) || num33.count(i)) {
+14                ans.insert(i);
+15            }
+16        }
+17        for (int& i : nums2) {
+18            if (num11.count(i) || num33.count(i)) {
+19                ans.insert(i);
+20            }
+21        }
+22        for (int& i : nums3) {
+23            if (num22.count(i) || num11.count(i)) {
+24                ans.insert(i);
+25            }
+26        }
+27
+28        vector<int> result(ans.begin(), ans.end());
+29
+30        return result;
+31    }
+32};
