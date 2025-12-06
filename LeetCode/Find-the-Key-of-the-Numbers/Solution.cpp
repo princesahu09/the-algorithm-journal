f639b024f656923fdbe0878742cb266e364dfbcc
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-06 18:22:14 +0530

    Add solution for Find the Key of the Numbers

--- LeetCode/Find-the-Key-of-the-Numbers/Solution.cpp
+++ LeetCode/Find-the-Key-of-the-Numbers/Solution.cpp
@@ -1,50 +1,30 @@
 1class Solution {
 2public:
-3
-4string  append(string s)
-5{
-6    int i=s.size();
-7    if(i==1)
-8    {
-9        s="000"+s;
-10    }
-11    else if(i==2)
-12    {
-13        s="00"+s;
-14    }
-15    else if(i==3)
-16    {
-17        s="0"+s;
-18    }
-19   
-20    
-21    return s;
-22}
-23    int generateKey(int num1, int num2, int num3) {
-24        string a=to_string(num1);
-25        string b=to_string(num2);
-26        string c=to_string(num3);
-27        a=append(a);
-28        cout<<a<<endl;
-29        b=append(b);
-30        cout<<b<<endl;
-31
-32        c=append(c);
-33        cout<<c<<endl;
-34
-35       string ans="";
-36
-37        for(int i=0;i<4;i++)
-38        {
-39
-40            ans+=min(a[i],min(b[i],c[i]));
-41
-42        }
-43
-44        return stoi(ans);
-45
-46
-47
-48
-49    }
-50};
+3    int generateKey(int num1, int num2, int num3) {
+4        int ans = 0;
+5        int i = 0;
+6        int temp = 0;
+7        while (i < 4) {
+8            int r1 = 0, r2 = 0, r3 = 0;
+9            if (num1 > 0) {
+10                r1 = num1 % 10;
+11                num1 /= 10;
+12            }
+13            if (num2 > 0) {
+14                r2 = num2 % 10;
+15                num2 /= 10;
+16            }
+17            if (num3 > 0) {
+18                r3 = num3 % 10;
+19                num3 /= 10;
+20            }
+21            int low = min(r1, min(r2, r3));
+22            ans+=low*pow(10,i);
+23            i++;
+24
+25           
+26        }
+27       
+28        return ans;
+29    }
+30};
