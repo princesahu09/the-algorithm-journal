ce81e711bf0474211fc511320fe633e7da778462
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-07 01:01:19 +0530

    Add solution for Find the K-or of an Array

--- LeetCode/Find-the-K-or-of-an-Array/Solution.cpp
+++ LeetCode/Find-the-K-or-of-an-Array/Solution.cpp
@@ -1,29 +1,30 @@
 1class Solution {
 2public:
 3    int findKOr(vector<int>& nums, int k) {
-4        unordered_map<int, int> bitone;
+4       vector<int>bitone(32,0);
 5
-6        for (int    &i : nums) {
-7            int pos = 0;
-8            while (i > 0) {
-9                if (i & 1) {
-10                    bitone[pos]++;
-11                }
-12                pos++;
-13                i >>= 1;
-14            }
-15        }
-16        int ans=0;
-17
-18        for(auto &i:bitone)
-19        {
-20            if(i.second>=k)
-21            {
-22                ans|=(1<<i.first);
-23            }
-24
+6       for(int &i:nums)
+7       {
+8        int pos=0;
+9        while(i>0)
+10        {
+11            if(i&1){
+12                bitone[pos]+=1;
+13            }
+14            pos++;
+15            i>>=1;
+16        }
+17       }
+18
+19       int ans=0;
+20       for(int i=0;i<32;i++)
+21       {
+22        if(bitone[i]>=k)
+23        {
+24            ans|=(1<<i);
 25        }
-26
-27        return ans;
-28    }
-29};
+26       }
+27
+28        return ans;
+29    }
+30};
