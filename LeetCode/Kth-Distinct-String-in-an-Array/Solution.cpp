d42482ad521eff7ab083e2c3acae6eac3ee17b86
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-07 20:52:14 +0530

    Add solution for Kth Distinct String in an Array

--- LeetCode/Kth-Distinct-String-in-an-Array/Solution.cpp
+++ LeetCode/Kth-Distinct-String-in-an-Array/Solution.cpp
@@ -3,22 +3,23 @@
 3    string kthDistinct(vector<string>& arr, int k) {
 4
 5        unordered_map<string, int> freq;
-6       
-7        for (auto& i : arr) {
-8            freq[i]++;
-9        }
-10
-11        for (string& i : arr) {
-12
-13            if (freq[i] == 1) {
-14                k--;
-15                if (k == 0) {
-16                    return i;
-17                }
-18    
-19            }
-20        }
-21
-22        return "";
-23    }
-24};
+6        freq.reserve(arr.size());
+7       
+8        for (auto& i : arr) {
+9            freq[i]++;
+10        }
+11
+12        for (string& i : arr) {
+13
+14            if (freq[i] == 1) {
+15                k--;
+16                if (k == 0) {
+17                    return i;
+18                }
+19    
+20            }
+21        }
+22
+23        return "";
+24    }
+25};
