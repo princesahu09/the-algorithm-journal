802b8bc92ff788fc3f318822cc2913a234d9fea9
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-07 21:07:39 +0530

    Add solution for Split the Array

--- LeetCode/Split-the-Array/Solution.cpp
+++ LeetCode/Split-the-Array/Solution.cpp
@@ -7,17 +7,22 @@
 7        for(int &i:nums)
 8        {
 9            freq[i]++;
-10        }
+10            if(freq[i]>2)
 11
-12        for(auto &i:freq)
-13        {
-14            if(i.second>2)
-15            {
-16                return false;
-17            }
-18        }
-19
-20        return true;
-21        
-22    }
-23};
+12            {
+13                return false;
+14            }
+15        }
+16
+17        // for(auto &i:freq)
+18        // {
+19        //     if(i.second>2)
+20        //     {
+21        //         return false;
+22        //     }
+23        // }
+24
+25        return true;
+26        
+27    }
+28};
