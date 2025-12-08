9f28162009e3dc28d8b6c2c877836a112f68ec60
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-08 14:15:57 +0530

    Add solution for Count Square Sum Triples

--- LeetCode/Count-Square-Sum-Triples/Solution.cpp
+++ LeetCode/Count-Square-Sum-Triples/Solution.cpp
@@ -1,19 +1,8 @@
-1class Solution {
-2public:
-3    int countTriples(int n) {
-4        int ans = 0;
-5
-6        for (int a = 1; a <= n; a++) {
-7            for (int b =  1; b <= n; b++) {
-8                for (int c =  1; c <= n; c++) {
-9                    if (a * a + b * b == c * c) {
-10                        ans++;
-11                    }
-12                }
-13            }
-14        }
-15
-16        return ans;
-17    }
-18};
+1
+150
+249
 19
+193
+148
+239
+250
