2de6bf5953517f84f674d4dcf211bd97c5a6603e
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-09 11:13:42 +0530

    Add solution for Array Partition

---
+++ LeetCode/Array-Partition/Solution.cpp
@@ -0,0 +1,18 @@
+1class Solution {
+2public:
+3    int arrayPairSum(vector<int>& nums) {
+4
+5        sort(nums.begin(),nums.end());
+6
+7        int ans=0;
+8
+9        for(int i=0;i+1<nums.size();i+=2)
+10        {
+11            ans+=nums[i];
+12           
+13            
+14        }
+15        return ans;
+16        
+17    }
+18};
