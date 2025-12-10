2b815ace30790e443e01c798cb15cb8e81e2538d
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-10 18:07:37 +0530

    Add solution for Split With Minimum Sum

--- LeetCode/Split-With-Minimum-Sum/Solution.cpp
+++ LeetCode/Split-With-Minimum-Sum/Solution.cpp
@@ -1,1 +1,29 @@
-num1
+1class Solution {
+2public:
+3    int splitNum(int num) {
+4      string str=to_string(num);
+5        sort(str.begin(),str.end());
+6        string num1="";
+7        string num2="";
+8
+9       for(int i=0;i<str.size();i++)
+10       {
+11        char ch=str[i];
+12        if(i%2==0)
+13        {
+14            num1+=ch;
+15        }
+16        else
+17        {
+18            num2+=ch;
+19        }
+20       }
+21       
+22        int sum=stoi(num1)+stoi(num2);
+23        return sum;
+24        
+25
+26
+27        
+28    }
+29};
