58ad2989c51ee82bb1b282056ca2f3776e5ec81c
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-10 21:15:48 +0530

    Add solution for Snake in Matrix

---
+++ LeetCode/Snake-in-Matrix/Solution.cpp
@@ -0,0 +1,22 @@
+1class Solution {
+2public:
+3    int finalPositionOfSnake(int n, vector<string>& commands) {
+4
+5        int row = 0;
+6        int col = 0;
+7        for (string& i : commands) {
+8            if (i == "UP") {
+9                row--;
+10
+11            } else if (i == "DOWN") {
+12                row++;
+13            } else if (i == "RIGHT") {
+14                col++;
+15            } else {
+16                col--;
+17            }
+18        }
+19
+20        return row*n+col;
+21    }
+22};
