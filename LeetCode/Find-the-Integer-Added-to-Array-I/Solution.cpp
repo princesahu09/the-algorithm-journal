d30ce37e04f8221b1e6d2afbadf94ddd907918cd
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-10 21:10:00 +0530

    Add solution for Find the Integer Added to Array I

--- LeetCode/Find-the-Integer-Added-to-Array-I/Solution.cpp
+++ LeetCode/Find-the-Integer-Added-to-Array-I/Solution.cpp
@@ -1,9 +1,9 @@
 1class Solution {
 2public:
 3    int addedInteger(vector<int>& num1, vector<int>& num2) {
-4        sort(num1.begin(),num1.end());
-5        sort(num2.begin(),num2.end());
-6        if(num1==num2)return 0;
+4        if(num1==num2)return 0;
+5        sort(num1.begin(),num1.end());
+6        sort(num2.begin(),num2.end());
 7
 8        return num2[0]-num1[0];
 9
