cc7c99bda490d5e10c1477a71f5a6e63b2fe368c
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-13 20:04:30 +0530

    Add solution for Find Smallest Letter Greater Than Target

--- LeetCode/Find-Smallest-Letter-Greater-Than-Target/Solution.cpp
+++ LeetCode/Find-Smallest-Letter-Greater-Than-Target/Solution.cpp
@@ -2,17 +2,31 @@
 2public:
 3    char nextGreatestLetter(vector<char>& letters, char target) {
 4
-5        char ans=letters[0];
-6        sort(letters.begin(),letters.end());
-7        for(int i=0;i<letters.size();i++)
-8        {
-9            if(letters[i]>target)
-10            {
-11                ans=letters[i];
-12                break;
-13            }
-14        }
-15        return ans;
-16        
-17    }
-18};
+5       
+6        int bitmask=0;
+7        char ans=letters[0];
+8
+9        for(char &ch:letters)
+10        {
+11            bitmask|=(1<<(ch-'a'));
+12        }
+13
+14        int searcher =target-'a';
+15        searcher+=1;
+16        while(searcher<26)
+17        {
+18          
+19            if(bitmask&(1<<searcher))
+20            {
+21                return char(searcher+'a');
+22             
+23            }
+24            searcher++;
+25
+26        }
+27
+28        return letters[0];
+29
+30        
+31    }
+32};
