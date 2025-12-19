8749879e40b25170533228f3ca26e307c8109ba0
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-19 12:01:30 +0530

    Add solution for Sort the Students by Their Kth Score

---
+++ LeetCode/Sort-the-Students-by-Their-Kth-Score/Solution.cpp
@@ -0,0 +1,10 @@
+1class Solution {
+2public:
+3    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+4
+5        sort(score.begin(),score.end(),[k](auto &a,auto &b){return a[k]>b[k];});
+6
+7        return score;
+8        
+9    }
+10};
