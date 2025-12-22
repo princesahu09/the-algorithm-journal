c02ca80686886d7129ef7955eef2afc88bc19384
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-22 10:32:06 +0530

    Add solution for Largest Perimeter Triangle

--- LeetCode/Largest-Perimeter-Triangle/Solution.cpp
+++ LeetCode/Largest-Perimeter-Triangle/Solution.cpp
@@ -3,21 +3,22 @@
 3    int largestPerimeter(vector<int>& nums) {
 4        sort(nums.begin(),nums.end());
 5        int ans=0;
-6
-7        for(int i=2;i<nums.size();i++)
-8        {
-9            int a=nums[i-2];
-10            int b=nums[i-1];
-11            int c=nums[i];
-12
-13            if((a+b>c)&&(a+c>b)&&(b+c>a))
-14            {
-15                ans=max(ans,a+b+c);
-16            }
-17
-18        }
-19
-20        return ans;
-21        
-22    }
-23};
+6        int a=0,b=0,c=0;
+7
+8        for(int i=2;i<nums.size();i++)
+9        {
+10             a=nums[i-2];
+11             b=nums[i-1];
+12             c=nums[i];
+13
+14            if((a+b>c)&&(a+c>b)&&(b+c>a))
+15            {
+16                ans=max(ans,a+b+c);
+17            }
+18
+19        }
+20
+21        return ans;
+22        
+23    }
+24};
