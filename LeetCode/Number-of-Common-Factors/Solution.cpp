61ae99109a3f54e8b7dff6a2f96a0dea27b3e7ec
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-22 10:23:24 +0530

    Add solution for Number of Common Factors

--- LeetCode/Number-of-Common-Factors/Solution.cpp
+++ LeetCode/Number-of-Common-Factors/Solution.cpp
@@ -1,17 +1,17 @@
-class Solution {
-public:
-    int commonFactors(int a, int b) {
-        int ans=0;
-
-        for(int i=1;i<=min(a,b);i++)
-        {
-            if(a%i==0&&b%i==0)
-            {
-                ans+=1;
-            }
-
-        }
-        return ans;
-        
-    }
-};
+1class Solution {
+2public:
+3    int commonFactors(int a, int b) {
+4        int ans=0;
+5
+6        for(int i=1;i<=min(a,b);i++)
+7        {
+8            if(a%i==0&&b%i==0)
+9            {
+10                ans+=1;
+11            }
+12
+13        }
+14        return ans;
+15        
+16    }
+17};
