29b077caccbee8d7ae813ca6ca6687d48e4f65a7
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-23 05:03:55 +0530

    Add solution for Number of Laser Beams in a Bank

--- LeetCode/Number-of-Laser-Beams-in-a-Bank/Solution.cpp
+++ LeetCode/Number-of-Laser-Beams-in-a-Bank/Solution.cpp
@@ -1,6 +1,6 @@
 1class Solution {
 2public:
-3    int NumberOfDevice(string laser) {
+3    int NumberOfDevice(string &laser) {
 4        int counter = 0;
 5        for (char &i : laser) {
 6            if (i == '1') {
@@ -9,28 +9,21 @@
 9        }
 10        return counter;
 11    }
-12    int numberOfBeams(vector<string>& bank){
+12    int numberOfBeams(vector<string>& bank) {
 13
-14        const int n=bank.size();
-15        
-16        int ans=0;
-17        int prev=0;
+14        const int n = bank.size();
+15
+16        int ans = 0;
+17        int prev = 0;
 18
-19        for(int i=0;i<n;i++)
-20        {
-21            int curr=NumberOfDevice(bank[i]);
-22            if(curr>0)
-23            {
-24                ans+=prev*curr;
-25                prev=curr;
+19        for (int i = 0; i < n; i++) {
+20            int curr = NumberOfDevice(bank[i]);
+21            if (curr > 0) {
+22                ans += prev * curr;
+23                prev = curr;
+24            }
+25        }
 26
-27            }
-28        }
-29
-30       
-31        
-32
-33
-34        return ans;
-35    }
-36};
+27        return ans;
+28    }
+29};
