004a10818ee9390b10677c87e05effcaf3344559
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-25 20:43:00 +0530

    Add solution for Minimum Moves to Equal Array Elements II

--- LeetCode/Minimum-Moves-to-Equal-Array-Elements-II/Solution.cpp
+++ LeetCode/Minimum-Moves-to-Equal-Array-Elements-II/Solution.cpp
@@ -4,14 +4,14 @@
 4        sort(nums.begin(),nums.end());
 5
 6        const int n=nums.size();
-7        int mid=n/2;
+7     
 8
 9     
 10        int move=0;
 11
 12        for(auto &i:nums)
 13        {
-14            move+=abs(i-nums[mid]);
+14            move+=abs(i-nums[n/2]);
 15        }
 16        return move;
 17        
