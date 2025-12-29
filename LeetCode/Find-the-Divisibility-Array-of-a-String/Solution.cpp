1ab924a061fc70f502debb6a25e796f6719c1e6f
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-12-29 22:23:18 +0530

    Add solution for Find the Divisibility Array of a String

---
+++ LeetCode/Find-the-Divisibility-Array-of-a-String/Solution.cpp
@@ -0,0 +1,25 @@
+1class Solution {
+2public:
+3    vector<int> divisibilityArray(string word, int m) {
+4
+5
+6        vector<int>prefix(word.size(),0);
+7        typedef long long ll;
+8
+9
+10        ll number=0;
+11        for(int i=0;i<word.size();i++)
+12        {
+13            number=(number*10+(word[i]-'0'))%m;
+14            if(number%m==0)
+15            {
+16                prefix[i]=1;
+17
+18            }
+19            
+20        }
+21
+22        return prefix;
+23        
+24    }
+25};
