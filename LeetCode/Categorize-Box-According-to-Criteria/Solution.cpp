6afa8ea9ffaea072bcbd027ba0b21f348e8d3594
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-01-04 05:25:47 +0530

    Add solution for Categorize Box According to Criteria

--- LeetCode/Categorize-Box-According-to-Criteria/Solution.cpp
+++ LeetCode/Categorize-Box-According-to-Criteria/Solution.cpp
@@ -6,23 +6,25 @@
 6        bool h = false;
 7
 8        typedef long long ll;
-9        int m = 1e4;
+9        const int m = 1e4;
 10
 11        ll vol = (ll)length * width * height;
 12
-13        if (vol >= 1e9 || length >= m || height >= m || width >=m) {
-14            b=true;
+13        if (vol >= 1e9 || length >= m || height >= m || width >= m) {
+14            b = true;
 15        }
 16
 17        if (mass >= 100) {
 18            h = true;
 19        }
 20
-21        if(b&h)return "Both";
-22        else if(!b&h)return "Heavy";
-23        else if(b&!h)return "Bulky";
-24        else return "Neither";
-25
-26        
-27    }
-28};
+21        if (b & h)
+22            return "Both";
+23        else if (!b & h)
+24            return "Heavy";
+25        else if (b & !h)
+26            return "Bulky";
+27        else
+28            return "Neither";
+29    }
+30};
