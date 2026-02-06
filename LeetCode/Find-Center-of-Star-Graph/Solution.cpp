545c4767c0586db3ee3e0edd8bd19f2a82c95e1c
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-06 13:09:35 +0530

    Add solution for Find Center of Star Graph

--- LeetCode/Find-Center-of-Star-Graph/Solution.cpp
+++ LeetCode/Find-Center-of-Star-Graph/Solution.cpp
@@ -3,15 +3,15 @@
 3    int findCenter(vector<vector<int>>& edges) {
 4        const int n=edges.size();
 5
-6        unordered_map<int,list<int>>adjlist;
+6        unordered_map<int,set<int>>adjlist;
 7
 8        for(int i=0;i<edges.size();i++)
 9        {
 10            int u=edges[i][0];
 11            int v=edges[i][1];
 12
-13            adjlist[u].push_back(v);
-14            adjlist[v].push_back(u);
+13            adjlist[u].insert(v);
+14            adjlist[v].insert(u);
 15        }
 16
 17        int counter=0;
