8515b7df7680cfb99ff8fbebbc3def3bf7b265bb
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-15 20:48:02 +0530

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.cpp
+++ LeetCode/Product-of-Array-Except-Self/Solution.cpp
@@ -2,51 +2,17 @@
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4
-5        int product=1;
+5        const int n = nums.size();
 6
-7        int zero=0;
-8       
-9        for(auto &i:nums)
-10        {
-11            if(i!=0)
-12            {product*=i;
-13
-14            }
-15            else
-16            {
-17                zero++;
-18            }
-19        }
-20        
-21            vector<int>ans(nums.size(),0);
-22
-23        if(zero>1)
-24        {
-25            return ans;
-26        }
-27        else if(zero==1)
-28        {
-29            for(int i=0;i<nums.size();i++)
-30            {
-31                if(nums[i]!=0)
-32                {
-33                   ans[i]=0;
-34                }
-35                else
-36                {
-37                    ans[i]=product;
-38                }
-39            }
-40        }
-41        else
-42        {
-43            for(int i=0;i<nums.size();i++)
-44            {
-45                ans[i]=product/nums[i];
-46            }
-47        }
-48
-49        return ans;
-50        
-51    }
-52};
+7        vector<int> ans(n, 1);
+8        for (int i = 1; i < n; i++) {
+9            ans[i] = ans[i - 1] * nums[i - 1];
+10        }
+11        int suffix = 1;
+12        for (int i = n - 2; i >= 0; i--) {
+13            suffix = suffix * nums[i + 1];
+14            ans[i] *= suffix;
+15        }
+16        return ans;
+17    }
+18};
