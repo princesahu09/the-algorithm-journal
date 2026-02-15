9de600c9898fb2f232523701c32f177fcf7e2298
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-15 20:41:33 +0530

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.cpp
+++ LeetCode/Product-of-Array-Except-Self/Solution.cpp
@@ -1,52 +1,52 @@
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-
-        int product=1;
-
-        int zero=0;
-       
-        for(auto &i:nums)
-        {
-            if(i!=0)
-            {product*=i;
-
-            }
-            else
-            {
-                zero++;
-            }
-        }
-        
-            vector<int>ans(nums.size(),0);
-
-        if(zero>1)
-        {
-            return ans;
-        }
-        else if(zero==1)
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)
-                {
-                   ans[i]=0;
-                }
-                else
-                {
-                    ans[i]=product;
-                }
-            }
-        }
-        else
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                ans[i]=product/nums[i];
-            }
-        }
-
-        return ans;
-        
-    }
-};
+1class Solution {
+2public:
+3    vector<int> productExceptSelf(vector<int>& nums) {
+4
+5        int product=1;
+6
+7        int zero=0;
+8       
+9        for(auto &i:nums)
+10        {
+11            if(i!=0)
+12            {product*=i;
+13
+14            }
+15            else
+16            {
+17                zero++;
+18            }
+19        }
+20        
+21            vector<int>ans(nums.size(),0);
+22
+23        if(zero>1)
+24        {
+25            return ans;
+26        }
+27        else if(zero==1)
+28        {
+29            for(int i=0;i<nums.size();i++)
+30            {
+31                if(nums[i]!=0)
+32                {
+33                   ans[i]=0;
+34                }
+35                else
+36                {
+37                    ans[i]=product;
+38                }
+39            }
+40        }
+41        else
+42        {
+43            for(int i=0;i<nums.size();i++)
+44            {
+45                ans[i]=product/nums[i];
+46            }
+47        }
+48
+49        return ans;
+50        
+51    }
+52};
