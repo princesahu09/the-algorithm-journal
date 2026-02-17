e17e37c69125c17a535abae7dbe6497ebf7584ef
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-18 03:32:20 +0530

    Add solution for Next Permutation

--- LeetCode/Next-Permutation/Solution.cpp
+++ LeetCode/Next-Permutation/Solution.cpp
@@ -1,26 +1,7 @@
 1class Solution {
 2public:
 3    void nextPermutation(vector<int>& nums) {
-4
-5        const int n = nums.size();
-6        int pivot = -1;
-7        for (int i = n - 2; i >=0; i--) {
-8            if (nums[i] < nums[i + 1]) {
-9                pivot = i;
-10                break;
-11            }
-12        }
-13
-14        if (pivot == -1) {
-15            reverse(nums.begin(), nums.end());
-16            return;
-17        }
-18        for (int i = n - 1; i >= 0; i--) {
-19            if (nums[i] > nums[pivot]) {
-20                swap(nums[i], nums[pivot]);
-21                break;
-22            }
-23        }
-24        reverse(nums.begin() + pivot + 1, nums.end());
-25    }
-26};
+4        next_permutation(nums.begin(),nums.end());
+5        
+6    }
+7};
