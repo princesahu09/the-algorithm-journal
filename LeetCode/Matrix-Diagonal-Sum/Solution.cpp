d3cf16fa9c3505a81c085e9dfc413412fd809df2
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-23 02:43:43 +0530

    Add solution for Matrix Diagonal Sum

--- LeetCode/Matrix-Diagonal-Sum/Solution.cpp
+++ LeetCode/Matrix-Diagonal-Sum/Solution.cpp
@@ -1,10 +1,19 @@
 1class Solution {
 2public:
-3    int diagonalSum(vector<vector<int>>& mat) {
-4        int n = mat.size();
-5        return accumulate(mat.begin(), mat.end(), 0, [i=0,n](int acc, auto &row) mutable {
-6            return acc + row[i] + row[n-1-i++];
-7        }) - (n%2 ? mat[n/2][n/2] : 0);
-8    }
-9};
-10
+3    int diagonalSum(vector<vector<int>>& nums) {
+4
+5
+6        int sum=0;
+7        const int n=nums.size();
+8        for(int i=0;i<nums.size();i++)
+9        
+10        {
+11            for(int j=0;j<nums.size();j++)
+12            if(i==j||i+j==n-1)
+13            {
+14                sum+=nums[i][j];
+15            }
+16        }
+17        return sum;
+18    }
+19};
