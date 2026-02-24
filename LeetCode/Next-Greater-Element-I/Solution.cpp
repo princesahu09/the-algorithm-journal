b521125827774a3a1d6cf05450cf6041cc7868cf
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-25 03:05:18 +0530

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.cpp
+++ LeetCode/Next-Greater-Element-I/Solution.cpp
@@ -1,26 +1,26 @@
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans(nums1.size());
-
-        unordered_map<int, int> greater;
-
-        for (int i = 0; i < nums2.size(); i++) {
-            int low = -1;
-            for (int j = i + 1; j < nums2.size(); j++) {
-                if (nums2[j] > nums2[i]) {
-                    low = nums2[j];
-                    break;
-                }
-            }
-            greater[nums2[i]] = low;
-        }
-
-        for (int i = 0; i < nums1.size(); i++) {
-            int element = nums1[i];
-            ans[i] = greater[element];
-        }
-
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+4        vector<int> ans(nums1.size());
+5
+6        unordered_map<int, int> greater;
+7
+8        for (int i = 0; i < nums2.size(); i++) {
+9            int low = -1;
+10            for (int j = i + 1; j < nums2.size(); j++) {
+11                if (nums2[j] > nums2[i]) {
+12                    low = nums2[j];
+13                    break;
+14                }
+15            }
+16            greater[nums2[i]] = low;
+17        }
+18
+19        for (int i = 0; i < nums1.size(); i++) {
+20            int element = nums1[i];
+21            ans[i] = greater[element];
+22        }
+23
+24        return ans;
+25    }
+26};
