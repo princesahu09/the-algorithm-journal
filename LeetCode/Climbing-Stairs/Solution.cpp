a0dfc93a85cadaa0e0c67e206a1d1072ed2de9f1
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-03-16 01:15:33 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.cpp
+++ LeetCode/Climbing-Stairs/Solution.cpp
@@ -3,15 +3,15 @@
 3    int climbStairs(int n) {
 4        if(n<=2)return n;
 5
-6        vector<int>t(n+1);
+6        vector<int>t(n);
 7
-8        t[1]=1;
-9        t[2]=2;
-10        for(int i=3;i<=n;i++)
+8        t[0]=1;
+9        t[1]=2;
+10        for(int i=2;i<n;i++)
 11        {
 12            t[i]=t[i-1]+t[i-2];
 13        }
-14        return t[n];
+14        return t[n-1];
 15        
 16    }
 17};
