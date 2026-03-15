7e70367273e0b80657f92e4ac372d100254696a1
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-03-16 01:07:20 +0530

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.cpp
+++ LeetCode/House-Robber/Solution.cpp
@@ -1,21 +1,21 @@
 1class Solution {
 2public:
-3    int rob(vector<int>& nums) {
-4
-5        const int n = nums.size();
-6
-7        vector<int> t(n);
-8
-9        if (n < 2) {
-10            return nums.front();
+3    int solve(vector<int>& nums, int n,vector<int>&dp) {
+4        if (n <= 0) {
+5            return 0;
+6        }
+7
+8        if(dp[n]!=-1)
+9        {
+10            return  dp[n];
 11        }
-12
-13        t[0] = nums[0];
-14        t[1] = max(nums[0], nums[1]);
-15
-16        for (int i = 2; i < n; i++) {
-17            t[i] = max(nums[i] + t[i - 2], t[i - 1]);
-18        }
-19        return t[n - 1];
-20    }
+12        dp[n]= max(nums[n - 1] + solve(nums, n - 2,dp), solve(nums, n - 1,dp));
+13        return dp[n];
+14    }
+15    int rob(vector<int>& nums) {
+16
+17        vector<int>dp(nums.size()+1,-1);
+18        
+19        
+20         return solve(nums, nums.size(),dp); }
 21};
