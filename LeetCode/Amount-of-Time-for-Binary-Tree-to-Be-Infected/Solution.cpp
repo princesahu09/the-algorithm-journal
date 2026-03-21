820276bb1ffb9ef6362edb77dead9302f727c883
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-03-21 23:09:35 +0530

    Add solution for Amount of Time for Binary Tree to Be Infected

---
+++ LeetCode/Amount-of-Time-for-Binary-Tree-to-Be-Infected/Solution.cpp
@@ -0,0 +1,62 @@
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13class Solution {
+14public:
+15    void generate(TreeNode* root, unordered_map<int, vector<int>>& adjList) {
+16        if (root == nullptr) {
+17            return;
+18        }
+19        if (root->left != nullptr) {
+20            adjList[root->val].push_back(root->left->val);
+21            adjList[root->left->val].push_back(root->val);
+22        }
+23        if (root->right != nullptr) {
+24            adjList[root->val].push_back(root->right->val);
+25            adjList[root->right->val].push_back(root->val);
+26        }
+27
+28        generate(root->left, adjList);
+29        generate(root->right, adjList);
+30    }
+31    int solve(unordered_map<int, vector<int>>& adjList, int start) {
+32        int ans = 0;
+33        queue<pair<int, int>> q;
+34        set<int> vis;
+35
+36        q.push({start, ans});
+37
+38        while (q.size() > 0) {
+39            int value = q.front().first;
+40            int time = q.front().second;
+41            q.pop();
+42
+43            if (!vis.count(value)) {
+44                vis.insert(value);
+45
+46                for (auto& i : adjList[value]) {
+47                    q.push({i, time + 1});
+48                }
+49            } else {
+50                continue;
+51            }
+52                ans = max(ans, time);
+53        }
+54        return ans;
+55    }
+56    int amountOfTime(TreeNode* root, int start) {
+57
+58        unordered_map<int, vector<int>> adjList;
+59        generate(root, adjList);
+60        return solve(adjList, start);
+61    }
+62};
