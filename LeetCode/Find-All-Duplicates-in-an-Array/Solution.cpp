764dd87ba2cb93f0d535a42bf7805ec4f6432c07
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-03-26 03:10:27 +0530

    Add solution for Find All Duplicates in an Array

--- LeetCode/Find-All-Duplicates-in-an-Array/Solution.cpp
+++ LeetCode/Find-All-Duplicates-in-an-Array/Solution.cpp
@@ -2,24 +2,20 @@
 2public:
 3    vector<int> findDuplicates(vector<int>& nums) {
 4
-5        vector<int>ans;
-6
-7        unordered_map<int,int>freq;
-8
-9        for(auto &i:nums)
-10        {
-11            freq[i]++;
-12        }
-13
-14        for(auto &i:freq)
-15        {
-16            if(i.second==2)
-17            {
-18                ans.push_back(i.first);
-19            }
-20        }
-21
-22        return ans;
-23        
-24    }
-25};
+5        vector<int> ans;
+6        int n = *max_element(nums.begin(), nums.end());
+7
+8        vector<int> freq(n + 1, 0);
+9        for (auto& i : nums) {
+10            freq[i]++;
+11        }
+12
+13        for (int i = 0; i <= n; i++) {
+14            if (freq[i] == 2) {
+15                ans.push_back(i);
+16            }
+17        }
+18
+19        return ans;
+20    }
+21};
