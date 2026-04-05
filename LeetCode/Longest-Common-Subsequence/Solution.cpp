4c2348eac810e2ae0c4072a98cab7cdda637124b
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-04-06 02:56:30 +0530

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.cpp
+++ LeetCode/Longest-Common-Subsequence/Solution.cpp
@@ -1,27 +1,46 @@
 1class Solution {
 2public:
-3    int solve(string &x, string &y, int n, int m, vector<vector<int>>& dp) {
-4        if (n == 0 || m == 0) {
-5            return dp[n][m]= 0;
-6        }
+3    int longestCommonSubsequence(string text1, string text2) {
+4
+5        int n=text1.size();
+6        int m=text2.size();
 7
-8        if (dp[n][m] != -1) {
-9            return dp[n][m];
-10        }
+8        vector<vector<int>>dp(n+1,vector<int>(m+1));
+9
+10        dp[0][0]=0;
 11
-12        if (x[n - 1] == y[m - 1]) {
-13            return dp[n][m] = 1 + solve(x, y, n - 1, m - 1, dp);
-14        }
-15
-16        return dp[n][m] =
-17                   max(solve(x, y, n, m - 1, dp), solve(x, y, n - 1, m, dp));
-18    }
-19    int longestCommonSubsequence(string text1, string text2) {
-20
-21        int n = text1.size();
-22        int m = text2.size();
-23        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-24
-25        return solve(text1, text2, text1.size(), text2.size(), dp);
-26    }
-27};
+12        for(int i=1;i<n+1;i++)
+13        {
+14            dp[i][0]=0;
+15        }
+16
+17        for(int j=1;j<m+1;j++)
+18        {
+19            dp[0][j]=0;
+20        }
+21
+22
+23        for(int i=1;i<n+1;i++)
+24        {
+25
+26
+27            for(int j=1;j<m+1;j++)
+28            {
+29                
+30
+31                if(text1[i-1]==text2[j-1])
+32                {
+33                    dp[i][j]=1+dp[i-1][j-1];
+34                }
+35                else
+36                {
+37                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+38                }
+39            }
+40        }
+41        return dp[n][m];
+42
+43
+44        
+45    }
+46};
