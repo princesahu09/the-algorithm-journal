9d31f0852bcfeb3b99752ac90ac28e9850b761b1
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-04-22 23:27:38 +0530

    Add solution for Check if Grid Satisfies Conditions

--- LeetCode/Check-if-Grid-Satisfies-Conditions/Solution.cpp
+++ LeetCode/Check-if-Grid-Satisfies-Conditions/Solution.cpp
@@ -1,32 +1,24 @@
 1class Solution {
 2public:
-3    bool solve(vector<vector<int>>& grid, int row, int col,
-4               vector<vector<int>>& dp) {
-5
-6        if (row >= grid.size() || col >= grid[0].size()) {
-7            return true;
-8        }
-9        if (row + 1 < grid.size() && grid[row][col] != grid[row + 1][col]) {
-10            return false;
-11        }
-12        if (col + 1 < grid[0].size() && grid[row][col] == grid[row][col + 1]) {
-13            return false;
-14        }
-15        if (dp[row][col] != -1) {
-16            return dp[row][col];
-17        }
-18
-19        bool down = solve(grid, row + 1, col, dp);
-20        bool right = solve(grid, row, col + 1, dp);
-21
-22        return dp[row][col] = down && right;
+3    bool satisfiesConditions(vector<vector<int>>& grid) {
+4
+5        int m=grid.size();
+6        int n=grid[0].size();
+7
+8
+9        for(int i=0;i<m;i++)
+10        {
+11            for(int j=0;j<n;j++)
+12            {
+13
+14                if(i+1<m&&grid[i+1][j]!=grid[i][j])return false;
+15                if(j+1<n&&grid[i][j+1]==grid[i][j])return false;
+16
+17
+18            }
+19        }
+20
+21        return true;
+22        
 23    }
-24    bool satisfiesConditions(vector<vector<int>>& grid) {
-25
-26        int m = grid.size();
-27        int n = grid[0].size();
-28        vector<vector<int>> dp(m, vector<int>(n, -1));
-29
-30        return solve(grid, 0, 0, dp);
-31    }
-32};
+24};
