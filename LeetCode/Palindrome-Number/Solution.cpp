fc0b309fda5b7a3636d95df531cafe62dc648881
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-04-23 04:23:23 +0530

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.cpp
+++ LeetCode/Palindrome-Number/Solution.cpp
@@ -1,22 +1,11 @@
-class Solution {
-public:
-    long reverse(long num, long ans) {
-        if (num == 0) {
-            return ans;
-        }
-
-        return reverse(num / 10, ans * 10 + (num % 10));
-    }
-    bool isPalindrome(int x) {
-        if(x<0)
-        {
-            return false;
-        }
-
-        if (x == reverse(x, 0)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-};
+1class Solution {
+2public:
+3    bool isPalindrome(int x) {
+4        
+5        string r=to_string(x);
+6        reverse(r.begin(),r.end());
+7        int rev=stol(r);
+8        return x==rev?true:false;
+9        
+10    }
+11};
