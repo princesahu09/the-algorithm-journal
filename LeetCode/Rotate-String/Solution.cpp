d14833ef216c7300d4dc5fbbba41ed3c56285a7a
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-05-03 11:37:26 +0530

    Add solution for Rotate String

--- LeetCode/Rotate-String/Solution.cpp
+++ LeetCode/Rotate-String/Solution.cpp
@@ -1,10 +1,10 @@
-class Solution {
-public:
-    bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size())return false;
-
-        s=s+s;
-        return s.find(goal)!=string::npos?true:false;
-        
-    }
-};
+1class Solution {
+2public:
+3    bool rotateString(string s, string goal) {
+4        if(s.size()!=goal.size())return false;
+5
+6        s=s+s;
+7        return s.find(goal)!=string::npos?true:false;
+8        
+9    }
+10};
