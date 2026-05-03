1f6fe1b40b3d0d907d30804b9fb3b0e06c78e316
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-05-03 10:56:23 +0530

    Add solution for Shortest Path in Binary Matrix

--- LeetCode/Shortest-Path-in-Binary-Matrix/Solution.cpp
+++ LeetCode/Shortest-Path-in-Binary-Matrix/Solution.cpp
@@ -7,40 +7,41 @@
 7
 8        int n = grid.size();
 9
-10       // vector<vector<bool>> visited(n, vector<bool>(n, false));
+10        // vector<vector<bool>> visited(n, vector<bool>(n, false));
 11
-12        queue<pair<pair<int, int>, int>> q;
+12        queue<pair<int, int>> q;
 13        vector<pair<int, int>> move = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
 14                                       {0, 1},   {1, -1}, {1, 0},  {1, 1}};
-15        q.push({{0, 0}, 1});
-16        grid[0][0]=1;
-17       // visited[0][0] = true;
-18
-19        while (q.size() > 0) {
-20            pair<int, int> point = q.front().first;
-21            int path = q.front().second;
-22            int i = point.first;
-23            int j = point.second;
-24            q.pop();
-25            if (i == n - 1 && j == n - 1) {
-26                return path;
-27            }
-28
-29            for (auto& m : move) {
-30                int x = i + m.first;
-31                int y = j + m.second;
-32
-33                if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0) {
-34
-35                   
-36                        grid[x][y]=1;
-37                        q.push({{x, y}, path + 1});
-38                      //  visited[x][y] = true;
-39                    
+15        q.push({0, 0});
+16        grid[0][0] = 1;
+17        // visited[0][0] = true;
+18        int step = 0;
+19
+20        while (q.size() > 0) {
+21            int count = q.size();
+22            while (count--) {
+23                int i = q.front().first;
+24                int j = q.front().second;
+25                q.pop();
+26                if (i == n - 1 && j == n - 1) {
+27                    return step + 1;
+28                }
+29
+30                for (auto& m : move) {
+31                    int x = i + m.first;
+32                    int y = j + m.second;
+33
+34                    if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0) {
+35
+36                        grid[x][y] = 1;
+37                        q.push({x, y});
+38                        //  visited[x][y] = true;
+39                    }
 40                }
 41            }
-42        }
-43
-44        return -1;
-45    }
-46};
+42            step++;
+43        }
+44
+45        return -1;
+46    }
+47};
