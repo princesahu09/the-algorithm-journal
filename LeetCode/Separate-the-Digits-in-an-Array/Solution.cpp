43f008618088c957afb50a9141029f6d5669e57f
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-05-11 07:48:28 +0530

    Add solution for Separate the Digits in an Array

--- LeetCode/Separate-the-Digits-in-an-Array/Solution.cpp
+++ LeetCode/Separate-the-Digits-in-an-Array/Solution.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-    vector<int> separateDigits(vector<int>& nums) {
-        vector<int>ans;
-
-
-        for(int i=0;i<nums.size();i++)
-        {
-            vector<int>demo;
-
-            while(nums[i]>0)
-            {
-                demo.push_back(nums[i]%10);
-                nums[i]/=10;
-            }
-            reverse(demo.begin(),demo.end());
-            ans.insert(ans.end(),demo.begin(),demo.end());
-          
-        }
-        return ans;
-        
-    }
-};
+1class Solution {
+2public:
+3    vector<int> separateDigits(vector<int>& nums) {
+4        vector<int>ans;
+5
+6
+7        for(int i=0;i<nums.size();i++)
+8        {
+9            vector<int>demo;
+10
+11            while(nums[i]>0)
+12            {
+13                demo.push_back(nums[i]%10);
+14                nums[i]/=10;
+15            }
+16            reverse(demo.begin(),demo.end());
+17            ans.insert(ans.end(),demo.begin(),demo.end());
+18          
+19        }
+20        return ans;
+21        
+22    }
+23};
