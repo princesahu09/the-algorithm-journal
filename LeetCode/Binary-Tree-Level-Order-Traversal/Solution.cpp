252cdcee48c6fb8b140e307c2bc1b78bc449db4b
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-07-03 10:18:53 +0530

    Add solution for Binary Tree Level Order Traversal

--- LeetCode/Binary-Tree-Level-Order-Traversal/Solution.cpp
+++ LeetCode/Binary-Tree-Level-Order-Traversal/Solution.cpp
@@ -6,56 +6,31 @@
 6 *     TreeNode *right;
 7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
-10 * right(right) {}
-11 * };
-12 */
-13class Solution {
-14
-15private:
-16    void solve( vector<vector<int>>& ans, queue<TreeNode*>& q)
-17
-18    {
-19        
-20        vector<int>level;
-21        while(!q.empty())
-22        {
-23            TreeNode* temp=q.front();
-24            q.pop();
-25
-26            if(temp==nullptr)
-27            {
-28                ans.push_back(level);
-29                level.clear();
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13    private : void solve(TreeNode* root,vector<vector<int>>&ans,int level)
+14    {
+15        if(root==nullptr)return;
+16
+17        if(level==ans.size())
+18        {
+19            ans.push_back({});
+20        }
+21
+22        ans[level].push_back(root->val);
+23
+24        solve(root->left,ans,level+1);
+25        solve(root->right,ans,level+1);
+26    }
+27public:
+28    vector<vector<int>> levelOrder(TreeNode* root) {
+29
 30
-31                if(!q.empty())
-32                {
-33                    q.push(nullptr);
-34                }
-35            }
-36
-37            else
-38            {
-39                level.push_back(temp->val);
-40
-41                if(temp->left)q.push(temp->left);
-42                if(temp->right)q.push(temp->right);
-43            }
-44        }
-45    }
-46
-47public:
-48    vector<vector<int>> levelOrder(TreeNode* root) {
-49
-50        vector<vector<int>> ans;
-51
-52        if(root==nullptr)return ans;
-53       
-54        queue<TreeNode*> q;
-55        q.push(root);
-56        q.push(nullptr);
-57        solve( ans, q);
-58
-59        return ans;
-60    }
-61};
+31        vector<vector<int>>ans;
+32        solve(root,ans,0);
+33        return ans;
+34        
+35    }
+36};
