f00abba6b342be09228deac11daf25e82e3f9ad3
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-07-07 19:39:04 +0530

    Add solution for Data Stream as Disjoint Intervals

--- LeetCode/Data-Stream-as-Disjoint-Intervals/Solution.java
+++ LeetCode/Data-Stream-as-Disjoint-Intervals/Solution.java
@@ -1,88 +1,61 @@
 1class SummaryRanges {
-2    List<Integer> temp;
+2    TreeSet<Integer> temp;
 3
 4    public SummaryRanges() {
 5
-6        temp = new ArrayList<>();
+6        temp = new TreeSet<>();
 7
 8    }
 9
-10    public boolean check(int target) {
-11        int start = 0;
-12        int end = temp.size() - 1;
+10    public void addNum(int value) {
+11
+12        temp.add(value);
 13
-14        while (start <= end) {
-15            int mid = start + (end - start) / 2;
-16
-17            if (temp.get(mid) == target) {
-18                return true;
-19            }
-20
-21            else if (temp.get(mid) > target) {
-22                end = mid - 1;
-23            } else {
-24                start = mid + 1;
-25            }
-26        }
-27
-28        return false;
-29    }
-30
-31    public void addNum(int value) {
-32
-33        if (!check(value)) {
-34            temp.add(value);
-35
-36        }
-37        return;
-38
-39    }
-40
-41    public int[][] getIntervals() {
+14    }
+15
+16    public int[][] getIntervals() {
+17
+18        if (temp.size() == 0) {
+19            return new int[][] {};
+20        }
+21
+22        List<int[]> res = new ArrayList<>();
+23
+24        int l = -1;
+25        int r = -1;
+26
+27        for (Integer val : temp) {
+28
+29            if (l == -1 && r == -1) {
+30                l = val;
+31                r = val;
+32            } else if (val == r + 1) {
+33                r = val;
+34            } else {
+35                res.add(new int[] { l, r });
+36
+37                l = val;
+38                r = val;
+39
+40            }
+41        }
 42
-43        if (temp.size() == 0) {
-44            return new int[][] {};
-45        }
+43        res.add(new int[] { l, r });
+44
+45        int[][] ans = new int[res.size()][2];
 46
-47        List<int[]> res = new ArrayList<>();
-48
-49        Collections.sort(temp);
+47        for (int i = 0; i < res.size(); i++) {
+48            ans[i] = res.get(i);
+49        }
 50
-51        int l = -1;
-52        int r = -1;
-53
-54        for (int i = 0; i < temp.size(); i++) {
+51        return ans;
+52
+53    }
+54}
 55
-56            if (l == -1 && r == -1) {
-57                l = temp.get(i);
-58                r = temp.get(i);
-59            } else if (temp.get(i) == r + 1) {
-60                r = temp.get(i);
-61            } else {
-62                res.add(new int[] { l, r });
-63
-64                l = temp.get(i);
-65                r = temp.get(i);
-66
-67            }
-68        }
-69
-70        res.add(new int[] { l, r });
-71
-72        int[][] ans = new int[res.size()][2];
-73
-74        for (int i = 0; i < res.size(); i++) {
-75            ans[i] = res.get(i);
-76        }
-77
-78        return ans;
-79
-80    }
-81}
-82
-83/**
-84 * Your SummaryRanges object will be instantiated and called as such:
-85 * SummaryRanges obj = new SummaryRanges();
-86 * obj.addNum(value);
-87 * int[][] param_2 = obj.getIntervals();
-88 */
+56/**
+57 * Your SummaryRanges object will be instantiated and called as such:
+58 * SummaryRanges obj = new SummaryRanges();
+59 * obj.addNum(value);
+60 * int[][] param_2 = obj.getIntervals();
+61 */
