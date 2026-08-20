882a78809b8ed6031f98861bd7ffa8f1a78a1206
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-08-20 22:31:09 +0530

    Add solution for Return Length of Arguments Passed

--- LeetCode/Return-Length-of-Arguments-Passed/Solution.js
+++ LeetCode/Return-Length-of-Arguments-Passed/Solution.js
@@ -2,18 +2,17 @@
 2 * @param {...(null|boolean|number|string|Array|Object)} args
 3 * @return {number}
 4 */
-5var argumentsLength = function(...args) {
+5var argumentsLength = function (...args) {
 6
-7    var counter=0;
+7    return args.length;
 8
-9    for(var i=0;i<args.length;i++)counter++;
-10    return counter;
+9
+10
 11
 12
-13    
-14    
-15};
-16
-17/**
-18 * argumentsLength(1, 2, 3); // 3
-19 */
+13
+14};
+15
+16/**
+17 * argumentsLength(1, 2, 3); // 3
+18 */
