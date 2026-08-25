25724ac16e1f7c3fd64648ec82cb1bdc9107e81c
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-08-25 19:53:03 +0530

    Add solution for Find Minimum Diameter After Merging Two Trees

--- LeetCode/Find-Minimum-Diameter-After-Merging-Two-Trees/Solution.java
+++ LeetCode/Find-Minimum-Diameter-After-Merging-Two-Trees/Solution.java
@@ -1,84 +1,73 @@
 1class Solution {
 2
-3    class Pair {
-4        int node;
-5        int time;
-6
-7        Pair(int node, int time) {
-8            this.node = node;
-9            this.time = time;
-10        }
-11    }
-12
-13    Pair bfs(int start, List<List<Integer>> adjList, int n) {
-14
-15        boolean[] visited = new boolean[n];
-16        Arrays.fill(visited, false);
+3    int diameter(int[][] edge) {
+4        int n = edge.length + 1;
+5        List<List<Integer>> adjList = new ArrayList<>();
+6        for (int i = 0; i < n; i++) {
+7            adjList.add(new ArrayList<>());
+8        }
+9
+10        for (int i = 0; i < edge.length; i++) {
+11            int u = edge[i][0];
+12            int v = edge[i][1];
+13
+14            adjList.get(u).add(v);
+15            adjList.get(v).add(u);
+16        }
 17
-18        Queue<Pair> q = new ArrayDeque<>();
-19        Pair far = new Pair(start, 0);
-20        q.offer(far);
-21        visited[start] = true;
+18        int[] degree = new int[n];
+19        for (int i = 0; i < n; i++) {
+20            degree[i] = adjList.get(i).size();
+21        }
 22
-23        while (q.size() > 0) {
+23        Queue<Integer> q = new ArrayDeque<>();
 24
-25            Pair cur = q.poll();
-26            int time = cur.time;
-27            // visited[cur.node] = true;
-28            if (cur.time > far.time) {
-29                far = cur;
-30            }
-31            for (Integer v : adjList.get(cur.node)) {
-32                if (!visited[v]) {
-33                    visited[v] = true;
-34                    q.offer(new Pair(v, time + 1));
-35                }
+25        for (int i = 0; i < n; i++) {
+26            if (degree[i] == 1) {
+27                q.offer(i);
+28            }
+29        }
+30
+31        int radius = 0;
+32
+33        while (n > 2) {
+34
+35            int size = q.size();
 36
-37            }
-38
-39        }
+37            for (int i = 0; i < size; i++) {
+38                int node = q.poll();
+39                n--;
 40
-41        return far;
-42
-43    }
-44
-45    int diameter(int[][] edge) {
-46
-47        int n = edge.length + 1;
-48        List<List<Integer>> adjList = new ArrayList<>();
-49        for (int i = 0; i < n; i++) {
-50            adjList.add(new ArrayList<>());
+41                for (int v : adjList.get(node)) {
+42                    degree[v]--;
+43                    if (degree[v] == 1) {
+44                        q.offer(v);
+45                    }
+46                }
+47            }
+48
+49            radius++;
+50
 51        }
 52
-53        for (int i = 0; i < edge.length; i++) {
-54            int u = edge[i][0];
-55            int v = edge[i][1];
+53        if (n == 2) {
+54            return radius * 2 + 1;
+55        }
 56
-57            adjList.get(u).add(v);
-58            adjList.get(v).add(u);
-59        }
-60
-61        Pair first = bfs(0, adjList, n);
-62
-63        Pair far = bfs(first.node, adjList, n);
+57        return 2 * radius;
+58    }
+59
+60    public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
+61
+62        int d1 = diameter(edges1);
+63        int d2 = diameter(edges2);
 64
-65        return far.time;
-66
-67    }
-68
-69    public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
-70
-71        int d1 = diameter(edges1);
-72       // System.out.println(d1);
-73        int d2 = diameter(edges2);
-74       // System.out.println(d2);
-75
-76        int r1 = (d1 + 1) / 2;
-77        int r2 = (d2 + 1) / 2;
-78        int cross = r1 + 1 + r2;
-79        return Math.max(
-80                Math.max(d1, d2),
-81                cross);
-82
-83    }
-84}
+65        int r1 = (d1 + 1) / 2;
+66        int r2 = (d2 + 1) / 2;
+67        int cross = r1 + 1 + r2;
+68        return Math.max(
+69                Math.max(d1, d2),
+70                cross);
+71
+72    }
+73}
