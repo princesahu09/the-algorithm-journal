2fc0791b163f153745458d41224f61bcd1f81e67
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-09-06 10:55:34 +0530

    Add solution for Count Good Cyclic Rotations

--- LeetCode/Count-Good-Cyclic-Rotations/Solution.java
+++ LeetCode/Count-Good-Cyclic-Rotations/Solution.java
@@ -9,46 +9,40 @@
 9    public int countGoodRotations(int[] nums) {
 10        int n = nums.length;
 11
-12        List<Integer> arr = new ArrayList<>();
-13        for (int i : nums) {
-14            arr.add(i);
-15        }
-16        for (int i : nums) {
-17            arr.add(i);
-18        }
-19
-20        long first = 0;
-21        long second = 0;
-22        for (int i = 0; i < n; i++) {
-23            if (i < n / 2) {
-24                first += nums[i];
-25            } else {
-26                second += nums[i];
-27            }
-28
-29        }
+12       
+13
+14        long first = 0;
+15        long second = 0;
+16        for (int i = 0; i < n; i++) {
+17            if (i < n / 2) {
+18                first += nums[i];
+19            } else {
+20                second += nums[i];
+21            }
+22
+23        }
+24
+25        int ans = 0;
+26
+27        int i = 0;
+28        int j = n / 2;
+29        int k = n;
 30
-31        int ans = 0;
-32
-33        int i = 0;
-34        int j = n / 2;
-35        int k = n;
-36
-37        while (k < 2 * n) {
-38            if (first > second) {
-39                ans++;
-40            }
-41            first -= arr.get(i);
-42            first += arr.get(j);
-43            second -= arr.get(j);
-44            second += arr.get(k);
-45
-46            i++;
-47            j++;
-48            k++;
-49        }
-50
-51        return ans;
-52
-53    }
-54}
+31        while (k < 2 * n) {
+32            if (first > second) {
+33                ans++;
+34            }
+35            first -= nums[i%n];
+36            first += nums[j%n];
+37            second -= nums[j%n];
+38            second +=nums[k%n];
+39
+40            i++;
+41            j++;
+42            k++;
+43        }
+44
+45        return ans;
+46
+47    }
+48}
