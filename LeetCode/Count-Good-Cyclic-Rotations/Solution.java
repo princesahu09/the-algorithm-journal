2ab51d238029366f86a2f0a0ad8015e36d383293
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-09-06 10:56:12 +0530

    Add solution for Count Good Cyclic Rotations

--- LeetCode/Count-Good-Cyclic-Rotations/Solution.java
+++ LeetCode/Count-Good-Cyclic-Rotations/Solution.java
@@ -1,48 +1,40 @@
 1class Solution {
-2    void print(List<Integer> arr) {
-3        for (Integer i : arr) {
-4            System.out.print(i + " ");
-5        }
-6        System.out.println();
-7    }
-8
-9    public int countGoodRotations(int[] nums) {
-10        int n = nums.length;
-11
-12       
-13
-14        long first = 0;
-15        long second = 0;
-16        for (int i = 0; i < n; i++) {
-17            if (i < n / 2) {
-18                first += nums[i];
-19            } else {
-20                second += nums[i];
-21            }
+2
+3    public int countGoodRotations(int[] nums) {
+4        int n = nums.length;
+5
+6        long first = 0;
+7        long second = 0;
+8        for (int i = 0; i < n; i++) {
+9            if (i < n / 2) {
+10                first += nums[i];
+11            } else {
+12                second += nums[i];
+13            }
+14
+15        }
+16
+17        int ans = 0;
+18
+19        int i = 0;
+20        int j = n / 2;
+21        int k = n;
 22
-23        }
-24
-25        int ans = 0;
-26
-27        int i = 0;
-28        int j = n / 2;
-29        int k = n;
-30
-31        while (k < 2 * n) {
-32            if (first > second) {
-33                ans++;
-34            }
-35            first -= nums[i%n];
-36            first += nums[j%n];
-37            second -= nums[j%n];
-38            second +=nums[k%n];
-39
-40            i++;
-41            j++;
-42            k++;
-43        }
-44
-45        return ans;
-46
-47    }
-48}
+23        while (k < 2 * n) {
+24            if (first > second) {
+25                ans++;
+26            }
+27            first -= nums[i % n];
+28            first += nums[j % n];
+29            second -= nums[j % n];
+30            second += nums[k % n];
+31
+32            i++;
+33            j++;
+34            k++;
+35        }
+36
+37        return ans;
+38
+39    }
+40}
