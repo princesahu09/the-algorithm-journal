19ae96c78e755d8686b0970b428996789e062bdf
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-09-07 23:55:22 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.java
+++ LeetCode/Missing-Number/Solution.java
@@ -1,21 +1,18 @@
 1class Solution {
-2    public int missingNumber(int[] nums) {
-3
-4        boolean[] vis = new boolean[nums.length + 1];
+2public:
+3    int missingNumber(vector<int>& nums) {
+4        int ans=0;
 5
-6        int prefix = 0;
-7
-8        for (int i = 0; i < nums.length; i++) {
+6        for(auto i:nums)
+7        {
+8            ans^=i;
 9
-10            vis[nums[i]] = true;
-11
-12            while (prefix < nums.length && vis[prefix]) {
-13                prefix++;
-14
-15            }
-16        }
-17
-18        return prefix;
-19
-20    }
-21}
+10        }
+11        for(int i=1;i<nums.size()+1;i++)
+12        {
+13            ans^=i;
+14        }
+15        return ans;
+16        
+17    }
+18};
