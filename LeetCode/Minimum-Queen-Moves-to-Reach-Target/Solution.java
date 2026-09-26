6e75b13421ae1aee1660fab1325f967022adb3e2
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-09-27 01:52:53 +0530

    Add solution for Minimum Queen Moves to Reach Target

--- LeetCode/Minimum-Queen-Moves-to-Reach-Target/Solution.java
+++ LeetCode/Minimum-Queen-Moves-to-Reach-Target/Solution.java
@@ -4,18 +4,17 @@
 4        if (source[0] == target[0] && source[1] == target[1])
 5            return 0;
 6
-7        if (source[1] == target[1]) {
+7        else if (source[1] == target[1]) {
 8            return 1;
 9        }
 10
-11        if (source[0] == target[0]) {
+11        else if (source[0] == target[0]) {
 12            return 1;
-13        }
-14        if (Math.abs(source[0] - target[0]) == Math.abs(source[1] - target[1])) {
-15            return 1;
-16        }
-17
-18        return 2;
-19
-20    }
-21}
+13        } else if (Math.abs(source[0] - target[0]) == Math.abs(source[1] - target[1])) {
+14            return 1;
+15        }
+16
+17        return 2;
+18
+19    }
+20}
